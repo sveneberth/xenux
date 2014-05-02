25a50d09f388df14d7731f14166dff062ecac7af
Sven Eberth <Sven.Eberth@gmail.com>
2014-05-02 23:44:28 +0200

    javascript fixes

--- core/js/formatierungen.js
+++ core/js/formatierungen.js
@@ -1,105 +1,34 @@
-function atext(txt) {
-	document.form.text.value += txt;
+function insert(aTag, eTag) {
+	var input = document.form.text;
+	input.focus();
+	var start = input.selectionStart;
+	var end = input.selectionEnd;
+	var insText = input.value.substring(start, end);
+	input.value = input.value.substr(0, start) + aTag + insText + eTag + input.value.substr(end);
+	var pos;
+	if (insText.length == 0) {
+		pos = start + aTag.length;
+	} else {
+		pos = start + aTag.length + insText.length + eTag.length;
+	}
+	input.selectionStart = pos;
+	input.selectionEnd = pos; 
 }
-var texto = false;
-var fetto = false;
-var kursivo = false;
-var unterstricheno = false;
-var linko = false;
-var lauftexto = false;
-var bunto = false;
 function text() {
-	if(texto == false) {
-		var col = prompt("Farbe (englisch oder Hex-Wert):","black");
-		if(col != null) {
-			atext("<span style='color:"+col+"'>");
-			document.getElementById('textb').innerHTML = "farbiger Text *";
-			texto = true;
-		}
-	} else {
-		atext("</span>");
-		document.getElementById('textb').innerHTML = "farbiger Text";
-		texto = false;
-	}
-}
-function fett() {
-	if(fetto == false) {
-		atext("<b>");
-		document.getElementById('fettb').innerHTML = "F *";
-		fetto = true;
-	} else {
-		atext("</b>");
-		document.getElementById('fettb').innerHTML = "F";
-		fetto = false;
-	}
-}
-function kursiv() {
-	if(kursivo == false) {
-		atext("<i>");
-		document.getElementById('kursivb').innerHTML = "K *";
-		kursivo = true;
-	} else {
-		atext("</i>");
-		document.getElementById('kursivb').innerHTML = "K";
-		kursivo = false;
-	}
-}
-function unterstrichen() {
-	if(unterstricheno == false) {
-		atext("<u>");
-		document.getElementById('unterstrichenb').innerHTML = "U *";
-		unterstricheno = true;
-	} else {
-		atext("</u>");
-		document.getElementById('unterstrichenb').innerHTML = "U";
-		unetrstricheno = false;
+	var col = prompt("Farbe (englisch oder Hex-Wert):","black");
+	if(col != null) {
+		insert("<span style='color:"+col+"'>", "</span>")
 	}
 }
 function link() {
-	if(linko == false) {
-		var href = prompt("Adresse:","http://");
-		if(href != null) {
-			atext("<a href='"+href+"'>");
-			document.getElementById('linkb').innerHTML = "Link *";
-			linko = true;
-		}
-	} else {
-		atext("</a>");
-		document.getElementById('linkb').innerHTML = "Link";
-		linko = false;
+	var href = prompt("Adresse:","http://");
+	if(href != null) {
+		insert("<a href='"+href+"'>", "</a>");
 	}
 }
 function bild() {
 	var src = prompt("Adresse:","http://");
 	if(src != null) {
-		atext("<img src='"+src+"' />");
-	}
-}
-function lauftext() {
-	if(lauftexto == false) {
-		atext("<marquee>");
-		document.getElementById('lauftextb').innerHTML = "Lauftext *";
-		lauftexto = true;
-	} else {
-		atext("</marquee>");
-		document.getElementById('lauftextb').innerHTML = "Lauftext";
-		lauftexto = false;
-	}
-}
-function newline() {
-	atext("<br />");
-}
-function hr() {
-	atext("<hr/>");
-}
-function bunt() {
-	if(bunto == false) {
-		atext("[bunt]");
-		document.getElementById('bunt').innerHTML = "bunter Text *";
-		bunto = true;
-	} else {
-		atext("[/bunt]");
-		document.getElementById('bunt').innerHTML = "bunter Text";
-		bunto = false;
+		insert("<img src='"+src+"' />", "");
 	}
 }
