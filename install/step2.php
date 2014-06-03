82f892adebb21ce63eb78b9bb9d5a12e654886bf
Sven Eberth <Sven.Eberth@gmail.com>
2014-06-03 11:18:08 +0200

    fixes

--- install/step2.php
+++ install/step2.php
@@ -1,4 +1,4 @@
-<p>Für die Verwendung von Xenux ist ein laufender Apache- und MySQl-Server nötig, bitte überprüfen sie dies, bevor sie mit der Installation fortfahren, ansonsten könnte es zu Problemen kommen (falls sie einen Webserver gemietet haben, sind diese mesitens aktiv).</p>
+<p>Für die Verwendung von Xenux ist ein laufender Apache- und MySQl-Server nötig, bitte überprüfen sie dies, bevor sie mit der Installation fortfahren, ansonsten könnte es zu Problemen kommen (falls sie einen Webserver gemietet haben, sind diese meistens aktiv).</p>
 <?php
 $next = true;
 ?>
