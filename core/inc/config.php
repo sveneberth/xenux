f5ac9ebfffb06d1b64cc619f6ccedd5868d52b40
Sven Eberth <Sven.Eberth@gmail.com>
2015-01-12 17:18:47 +0100

    config

--- core/inc/config.php
+++ core/inc/config.php
@@ -1,34 +1,35 @@
 <?php
 session_start(); // start php session
 
-$XENUX_URL = 'http';
-if (@$_SERVER["HTTPS"] == "on") {$XENUX_URL .= "s";}
-$XENUX_URL .= "://";
-if ($_SERVER["SERVER_PORT"] != "80") {
-	$XENUX_URL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].dirname(dirname($_SERVER["SCRIPT_NAME"]));
-} else {
-	$XENUX_URL .= $_SERVER["SERVER_NAME"].dirname(dirname($_SERVER["SCRIPT_NAME"]));
+if($_SERVER['HTTP_HOST'] == 'localhost') {
+	error_reporting(E_ALL);
 }
 
-define('BASEDIR', dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR);
-define('XENUX_URL', $XENUX_URL);
+$url = 'http';
+if (@$_SERVER["HTTPS"] == "on") $url .= "s";
+$url .= "://";
+if ($_SERVER["SERVER_PORT"] != "80") {
+	$url .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . dirname(dirname($_SERVER["SCRIPT_NAME"]));
+} else {
+	$url .= $_SERVER["SERVER_NAME"] . dirname(dirname($_SERVER["SCRIPT_NAME"]));
+}
+
+define('BASEDIR', dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
+define('XENUX_URL', $url);
 
 include_once(BASEDIR.'core/inc/arrays.php'); // include arrays
 include_once(BASEDIR.'core/inc/functions.php'); // include functions
 include_once(BASEDIR.'mysql.conf'); // include Config for MySQL
 
-if($_SERVER['HTTP_HOST'] == 'localhost') {
-	error_reporting(E_ALL);
-}
 
 $db = new MySQLi(MYSQL_HOST, MYSQL_USER, MYSQL_PW, MYSQL_DB); // connect with database
 
 if(mysqli_connect_errno()) { // if conntection failed
-	printf("Can't connect to MySQL Server. Please tell the <a href=\"mailto:developers@xenux\">Devolopers</a> this Errorcode:<br />\"%s\"", mysqli_connect_error());
+	printf("Can't connect to MySQL Server. Please tell the administrator this Errorcode:<br />\"%s\"", mysqli_connect_error());
 	exit;
 }
 
-$db->query("SET NAMES 'utf8'"); // define database as utf-8
+$db->query("SET NAMES 'utf8';"); // define database as utf-8
 
 if(isset($_SESSION['login_xenux'])) {
 	if($_SESSION['login_xenux'] == 1) {
