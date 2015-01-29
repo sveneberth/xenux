0fe86eca4125a6a1a48dde5f20b4c4a6b11bc76c
Sven Eberth <Sven.Eberth@gmail.com>
2015-01-29 16:38:01 +0100

    typo

--- install/step6.php
+++ install/step6.php
@@ -1,6 +1,6 @@
 <p>Die Installation wurde erfolgreich abgeschlossen!</p>
 <p>Sie können ihre Homepage öffnen, sich einloggen und bearbeiten.</p>
-<p>Alle hier vorgenommenen Einstellungen können sie in der Datei "mysql.conf" Sowie im Editroom unter "Grundeinstellungen" ändern.</p>
+<p>Alle hier vorgenommenen Einstellungen können sie in der Datei "mysql.conf.php" Sowie im Editroom unter "Grundeinstellungen" ändern.</p>
 <p>Bitte löschen sie den Ordner "install", da sonst alle Einstellungen durch das erneute ausführen der Installation überschrieben werden. Nach dem löschen werden sie automatisch zur Startseite ihrer Homepage weiter geleitet.</p>
 <p>Viel Spaß!</p>
 <form action="" method="post">
