710169b614d8b4498554382cf5de605b3036bab2
Sven Eberth <Sven.Eberth@gmail.com>
2015-12-28 20:59:13 +0100

    renamed the unit of the output of formatBytes() (MB -> MIB, etc.)

--- core/inc/functions.php
+++ core/inc/functions.php
@@ -186,7 +186,7 @@ function formatBytes ($size, $precision = 2)
 	$base = log($size, 1024);
 	$suffixes = array('', 'k', 'M', 'G', 'T');
 
-	return round(pow(1024, $base - floor($base)), $precision) . ' ' . $suffixes[floor($base)] . 'B';
+	return round(pow(1024, $base - floor($base)), $precision) . ' ' . $suffixes[floor($base)] . 'iB';
 }
 
 
