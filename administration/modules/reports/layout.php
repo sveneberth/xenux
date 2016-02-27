7cf6937a4640eb30d153c9a8946747b456f5f0bf
Sven Eberth <Sven.Eberth@gmail.com>
2016-02-27 21:43:58 +0100

    removed report module

--- administration/modules/reports/layout.php
+++
@@ -1,8 +0,0 @@
-<section class="box-shadow floating one-column-box">
-	<p><?= __('choose a logfile') ?></p>
-	<select name="logFile" size="1" onchange="window.location='?file='+this.value">
-		{{logFiles}}
-	</select>
-
-	<textarea readonly class="full_page" style="height: 300px; resize: vertical;">{{log}}</textarea>
-</section>
